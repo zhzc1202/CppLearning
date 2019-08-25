0314d72fc9a36ff61d041e36a8623fe61be3685a
Jason Cheung <465317110@qq.com>
2019-08-25 17:56:57 +0800

    20190825

---
+++ Cpp/Day2/math.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	printf("7/3  = %d 7%%3   = %d\n",7/3,7%3);
+	printf("-7/3 = %d -7%%3  = %d\n",-7/3,-7%3);
+	printf("7/-3 = %d 7%%-3  = %d\n",7/-3,7%-3);
+	return 0;
+}
